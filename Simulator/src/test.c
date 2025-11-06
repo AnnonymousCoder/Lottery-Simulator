b2dbf6cb7e06986fa0044d048f7e41597104865f
AnnonymousCoder <matthewfebu21@gmail.com>
2025-11-05 19:19:08 -0500

    Added Readme file

--- Simulator/src/test.c
+++ Simulator/src/test.c
@@ -29,136 +29,47 @@
 #include <stdio.h>
 
 //------------------------------------------------------------------------------------
-// Program main entry point
+// Defines and Macros
+//------------------------------------------------------------------------------------
+#define FPS 60
+#define BODY_MASS 1.0f
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
+#define COEFF_OF_RESITUTION 0.7f
+#define GRAVITY_ACCELERATION 9.81f
+//------------------------------------------------------------------------------------
+// Global Variables Declarations
 //------------------------------------------------------------------------------------
 
-Vector2 acc;
-Vector2 vel;
-Vector2 pos;
-Vector2 force;
-Vector2 ref_vel; 
-Vector2 delta_vel;
 
-float grav_acc = 9.8;
-float mass = 5.0;
-double ref_time = 0.0;
-double delta_t = 0.0;
-
+//------------------------------------------------------------------------------------
+// Prototype Functions Declaration
+//------------------------------------------------------------------------------------
 void u_physics(Vector2);
 
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 1040;
-    const int screenHeight = 820;
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Lottery Simulator");
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetTargetFPS(FPS);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-    Vector2 momentum  = {0.0f, 0.0f};
+    while(!WindowShouldClose()){
 
-    bool apply_force = false;
-    float mag = 0.0f;
-    float angle = 0.0f;
-
-    pos.x = screenWidth/2;
-    pos.y = 28.0f;
-    vel   = Vector2Zero();
-    force = Vector2Zero();
-    ref_vel   = Vector2Zero();
-    delta_vel = Vector2Zero();
-
-    force.y = mass * grav_acc;  //Applying Gravity force
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        ref_vel = vel;
-
-        // if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-        //     apply_force = true;
-        //     float force_x = -mag * cosf(angle);
-        //     float force_y = -mag * sinf(angle);
-        //     // vel.x = (force_x * delta_t) / mass;
-        //     vel.y += (force_y * delta_t) / mass;
-        //     // printf("Velocity: (%.3f, %.3f), Force: (%.3f, %.3f), delta_t: %.6f\n", vel.x, vel.y, force_x, force_y, delta_t);
-        // }
-
-        if(!apply_force){
-            int px = GetMouseX() - screenWidth/2;
-            int py = GetMouseY() - screenHeight/2;
-            mag = sqrtf(px * px + py * py);
-            angle = atan2f(py, px);
-        }
-
-        if(pos.x + 25 > screenWidth || pos.x - 25 < 0.0f){
-            momentum.x = delta_vel.x * mass;
-            force.x += (-momentum.x / (delta_t*1e6));
-            vel.x *= -0.5f;
-            pos.x = (pos.x + 25 > screenWidth) ? screenWidth - 25 : 25;
-        }
-
-        if(pos.y + 25 > screenHeight || pos.y - 25 < 0.0f){
-            momentum.y = delta_vel.y * mass;
-            force.y += (-momentum.y / (delta_t*1e6));
-            vel.y *= -0.5f;
-            pos.y = (pos.y + 25 > screenHeight) ? screenHeight - 25 : 25;
-            // printf("Force: (%.3f, %.3f), Vel: (%.3f, %.3f), Momentum: (%.3f, %.3f), delta_vel: (%.6f, %.6f), delta_t: %.6f\n", force.x, force.y, vel.x, vel.y, momentum.x, momentum.y, delta_vel.x, delta_vel.y, delta_t);
-        }
-
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-            apply_force = true;
-            float force_x = -mag * cosf(angle) + force.x;
-            float force_y = -mag * sinf(angle) + force.y;
-            vel.y   += (force_y * delta_t*1e6) / mass;
-            vel.x   += (force_x * delta_t*1e6) / mass;
-            // printf("Velocity: (%.3f, %.3f), Force: (%.3f, %.3f), delta_t: %.6f\n", vel.x, vel.y, force_x, force_y, delta_t);
-        }
-
-        u_physics(force);
-        // printf("Force: (%.3f, %.3f), Vel: (%.3f, %.3f), Momentum: (%.3f, %.3f), delta_vel: (%.6f, %.6f), delta_t: %.6f\n", force.x, force.y, vel.x, vel.y, momentum.x, momentum.y, delta_vel.x, delta_vel.y, delta_t);
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
+        ClearBackground(RAYWHITE);
 
-            ClearBackground(RAYWHITE);
-
-            DrawText(TextFormat("pressed: %s, Mag : %.2f, Angle: %.3f, Force : (%.3f, %.3f), vel:(%.3f, %.3f)", apply_force ? "true" : "false", mag, angle, force.x, force.y, vel.x, vel.y), 10, 10, 20, LIGHTGRAY);
-            DrawLine(screenWidth/2, screenHeight/2, (-mag * cosf(angle)) + screenWidth/2, (-mag * sinf(angle)) + screenHeight/2, BLACK);
-            DrawCircleV(pos, 25, MAROON);
-
+        
+        
         EndDrawing();
-        ref_time = GetTime();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
 
 void u_physics(Vector2 force){
 
-    delta_t = GetTime() - ref_time;
-
-    acc.x += force.x / mass;
-    acc.y += force.y / mass;
-
-    vel.x += acc.x * delta_t;
-    vel.y += acc.y * delta_t;
-
-    pos.x += vel.x;
-    pos.y += vel.y;
-
-    delta_vel = Vector2Subtract(vel, ref_vel);
 }
